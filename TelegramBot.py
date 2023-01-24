f3bb85b44ecb5bf09ef10a17e06715dc957fc50c
Parag Kale <93380434+ParagKale09@users.noreply.github.com>
2023-01-24 22:39:28 +0530

    Update TelegramBot.py

--- TelegramBot.py
+++ TelegramBot.py
@@ -5,13 +5,28 @@ import os
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
+#Your Token from bot fahter
 TOKEN = "Your_Token"
+
+#List of Sharable PDFs
+pdf_files = ['Names_of_Sharable_PDF']
 
 def start(update, context):
     update.message.reply_text("Your_Message")
     
 def pdf(update, context):
+    global is_found
+    is_found = True                #defining Flag
+    file_name = " ".join(context.args)
+    for file in pdf_files:
+       if file == file_name:
+        update.message.reply_text("Getting your file...")
+        context.bot.sendDocument(update.effective_chat.id,document=open(file_name+'.pdf','rb'))
+        is_found = False
+        break                           #If File found Break the Loop
     
+    if is_found:
+        update.message.reply_text("Sorry, I don't have that PDF file. Please make sure you spelled the name correctly.") 
 
 
 updater = Updater(TOKEN, use_context=True)
